e7cb5b4eb828b47b96fd7d234ad606365d6f76e0
SeoJongwon <3338761@naver.com>
2024-07-23 09:52:37 +0900

    chore : 파일 서버 생성

---
+++ backend/file-server/src/test/java/com/ssafy/ozz/fileserver/FileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ozz.fileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
