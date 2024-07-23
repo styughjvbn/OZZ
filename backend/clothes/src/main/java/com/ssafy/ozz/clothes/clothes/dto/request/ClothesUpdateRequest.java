13ae29b56bab0616ee11a67fa9987f185d10fc3a
SeoJongwon <3338761@naver.com>
2024-07-23 14:42:12 +0900

    feat : 옷 수정 request dto 생성

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/clothes/dto/request/ClothesUpdateRequest.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/clothes/dto/request/ClothesUpdateRequest.java
@@ -1,6 +1,22 @@
 package com.ssafy.ozz.clothes.clothes.dto.request;
 
-public record ClothesUpdateRequest(
+import com.ssafy.ozz.clothes.clothes.properties.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ClothesUpdateRequest(
+        Long categoryLowId,
+        String name,
+        Size size,
+        Fit fit,
+        String memo,
+        String brand,
+        LocalDateTime purchaseDate,
+        String purchaseSite,
+        List<Color> colorList,
+        List<Texture> textureList,
+        List<Season> seasonList,
+        List<Style> styleList
 ) {
 }
