2e4806a9fc378164586363c0b99c9653404045b8
SeoJongwon <3338761@naver.com>
2024-07-24 16:50:47 +0900

    test: 코디 조회 기능 테스트
    - 코디 번호로 조회
    - 조건에 따른 코디 목록 조회

--- backend/clothes/src/test/java/com/ssafy/ozz/clothes/coordinate/service/CoordinateServiceImplTest.java
+++ backend/clothes/src/test/java/com/ssafy/ozz/clothes/coordinate/service/CoordinateServiceImplTest.java
@@ -9,15 +9,14 @@ import com.ssafy.ozz.clothes.coordinate.domain.Coordinate;
 import com.ssafy.ozz.clothes.coordinate.domain.CoordinateClothes;
 import com.ssafy.ozz.clothes.coordinate.dto.CoordinateClothesCreateRequest;
 import com.ssafy.ozz.clothes.coordinate.dto.CoordinateCreateRequest;
+import com.ssafy.ozz.clothes.coordinate.dto.SearchCondition;
 import com.ssafy.ozz.clothes.coordinate.repository.CoordinateClothesRepository;
-import com.ssafy.ozz.clothes.coordinate.repository.CoordinateRepository;
 import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
@@ -33,9 +32,6 @@ import static com.ssafy.ozz.clothes.global.util.EnumBitwiseConverter.toBits;
 class CoordinateServiceImplTest {
     @Autowired
     private CoordinateService coordinateService;
-
-    @Autowired
-    private CoordinateRepository coordinateRepository;
 
     @Autowired
     private ClothesRepository clothesRepository;
@@ -64,29 +60,64 @@ class CoordinateServiceImplTest {
         // given
         String name = "서터릿 코디";
         List<Style> styleList = Arrays.asList(Style.STREET, Style.CASUAL);
-        CoordinateCreateRequest request = CoordinateCreateRequest.builder()
-                .name(name)
-                .styleList(styleList)
-                .clothes(clothesList.stream().map(clothes ->
-                        CoordinateClothesCreateRequest.builder().clothesId(clothes.getClothesId()).offset(1).build()).toList())
-                .build();
+        CoordinateCreateRequest request = getCoordinateCreateRequest(name, styleList);
         // when
-        Long coordinateId = coordinateService.createCoordinate(userId, null, request);
+        Coordinate coordinate = coordinateService.createCoordinate(userId, null, request);
 
         // then
-        Coordinate coordinate = coordinateRepository.findById(coordinateId).orElseThrow();
         List<CoordinateClothes> coordinateClothes = coordinateClothesRepository.findByCoordinate(coordinate);
         Assertions.assertThat(coordinateClothes.size()).isEqualTo(clothesList.size());
         Assertions.assertThat(coordinate.getName()).isEqualTo(name);
         Assertions.assertThat(coordinate.getStyle()).isEqualTo(toBits(styleList));
     }
 
+    CoordinateCreateRequest getCoordinateCreateRequest(String name, List<Style> styleList) {
+        return CoordinateCreateRequest.builder()
+                .name(name)
+                .styleList(styleList)
+                .clothes(clothesList.stream().map(clothes ->
+                        CoordinateClothesCreateRequest.builder().clothesId(clothes.getClothesId()).offset(1).build()).toList())
+                .build();
+    }
+
     @Test
     void getCoordinate() {
+        // given
+        String name = "캐주얼 코디";
+        List<Style> styleList = Arrays.asList(Style.CASUAL, Style.CLASSIC);
+        CoordinateCreateRequest request = getCoordinateCreateRequest(name, styleList);
+        Long coordinateId = coordinateService.createCoordinate(userId, null, request).getCoordinateId();
+
+        // when
+        Coordinate coordinate = coordinateService.getCoordinate(coordinateId);
+
+        // then
+        Assertions.assertThat(coordinate.getCoordinateId()).isEqualTo(coordinateId);
     }
 
     @Test
     void getCoordinatesOfUser() {
+        // pagenation 적용 X
+        // given
+        String name = "캐주얼 코디";
+        List<Style> styleList = Arrays.asList(Style.CASUAL, Style.CLASSIC);
+        CoordinateCreateRequest request = getCoordinateCreateRequest(name, styleList);
+        coordinateService.createCoordinate(userId, null, request);
+
+        String name2 = "캐주얼 코디";
+        List<Style> styleList2 = Arrays.asList(Style.CASUAL, Style.STREET);
+        CoordinateCreateRequest request2 = getCoordinateCreateRequest(name2, styleList2);
+        coordinateService.createCoordinate(userId, null, request2);
+
+        SearchCondition condition = SearchCondition.builder()
+                .styleList(List.of(Style.CASUAL))
+                .build();
+
+        // when
+        List<Coordinate> coordinateList = coordinateService.getCoordinatesOfUser(userId, condition);
+
+        // then
+        Assertions.assertThat(coordinateList.size()).isEqualTo(2);
     }
 
     @Test
