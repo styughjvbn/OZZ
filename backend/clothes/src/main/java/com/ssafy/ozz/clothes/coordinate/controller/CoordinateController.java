87faf8119709017560ad800f787f2733953611a6
SeoJongwon <3338761@naver.com>
2024-07-24 17:52:59 +0900

    feat(S11P12A804-134): 코디 조회 API 구현 -
    - 코디 조회 API 구현
    - 내 코디 목록 조회 API 구현

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/controller/CoordinateController.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/controller/CoordinateController.java
@@ -1,8 +1,13 @@
 package com.ssafy.ozz.clothes.coordinate.controller;
 
+import com.ssafy.ozz.clothes.coordinate.domain.Coordinate;
 import com.ssafy.ozz.clothes.coordinate.dto.CoordinateCreateRequest;
+import com.ssafy.ozz.clothes.coordinate.dto.CoordinateResponse;
+import com.ssafy.ozz.clothes.coordinate.dto.SearchCondition;
 import com.ssafy.ozz.clothes.coordinate.service.CoordinateService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +25,17 @@ public class CoordinateController {
         Long userId = 1L;
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(coordinateService.createCoordinate(userId,imageFile,request).getCoordinateId());
+    }
+
+    @GetMapping("/{coordinateId}")
+    public ResponseEntity<CoordinateResponse> getCoordinate(@PathVariable final Long coordinateId) {
+        return ResponseEntity.ok(new CoordinateResponse(coordinateService.getCoordinate(coordinateId)));
+    }
+
+    @GetMapping
+    public ResponseEntity<Slice<CoordinateResponse>> getCoordinateList(@ModelAttribute SearchCondition condition, Pageable pageable) {
+        // TODO: 실제 유저 번호 받아오기
+        Long userId = 1L;
+        return ResponseEntity.ok(coordinateService.getCoordinatesOfUser(userId,condition,pageable).map(CoordinateResponse::new));
     }
 }
---
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/dto/CoordinateResponse.java
@@ -0,0 +1,32 @@
+package com.ssafy.ozz.clothes.coordinate.dto;
+
+import com.ssafy.ozz.clothes.clothes.dto.response.ClothesBasicResponse;
+import com.ssafy.ozz.clothes.clothes.dto.response.ClothesResponse;
+import com.ssafy.ozz.clothes.clothes.properties.Style;
+import com.ssafy.ozz.clothes.coordinate.domain.Coordinate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.ssafy.ozz.clothes.global.util.EnumBitwiseConverter.toEnums;
+
+public record CoordinateResponse (
+        String name,
+        List<Style> styleList,
+        LocalDateTime createdDate,
+        List<ClothesBasicResponse> clothesList,
+        // TODO: File DTO로 반환하기
+        Long imageFileId
+){
+    public CoordinateResponse(Coordinate coordinate){
+        this(
+            coordinate.getName(),
+            toEnums(Style.class, coordinate.getStyle()),
+            coordinate.getCreatedDate(),
+            coordinate.getCoordinateClothesList().stream().map(coordinateClothes ->
+                    new ClothesBasicResponse(coordinateClothes.getClothes())
+            ).toList(),
+            coordinate.getImageFileId()
+        );
+    }
+}
