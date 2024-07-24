fa223f7bd47393dfb163b4d6c1f2f3517d9d0e83
SeoJongwon <3338761@naver.com>
2024-07-24 14:30:23 +0900

    feat: 코디_옷 repo에서 코디로 옷 목록 조회하는 기능 구현

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/repository/CoordinateClothesRepository.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/repository/CoordinateClothesRepository.java
@@ -6,6 +6,9 @@ import com.ssafy.ozz.clothes.coordinate.domain.CoordinateClothesId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CoordinateClothesRepository extends JpaRepository<CoordinateClothes, CoordinateClothesId> {
+    List<CoordinateClothes> findByCoordinate(Coordinate coordinate);
 }
