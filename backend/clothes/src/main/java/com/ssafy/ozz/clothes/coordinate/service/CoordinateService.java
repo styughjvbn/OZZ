dd332a67953a4d25e984ebcb8bbe4c653e99a1ef
SeoJongwon <3338761@naver.com>
2024-07-24 12:44:37 +0900

    chore: 코디 관련 service interface 생성
    추가로 코디 관련 dto 생성

---
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/service/CoordinateService.java
@@ -0,0 +1,24 @@
+package com.ssafy.ozz.clothes.coordinate.service;
+
+import com.ssafy.ozz.clothes.coordinate.domain.Coordinate;
+import com.ssafy.ozz.clothes.coordinate.dto.CoordinateCreateRequest;
+import com.ssafy.ozz.clothes.coordinate.dto.CoordinateUpdateRequest;
+import com.ssafy.ozz.clothes.coordinate.dto.SearchCondition;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+import java.util.List;
+
+public interface CoordinateService {
+    Long createCoordinate(CoordinateCreateRequest request);
+
+    Coordinate getCoordinate(Long coordinateId);
+
+    List<Coordinate> getCoordinatesOfUser(Long userId, SearchCondition condition);
+
+    Slice<Coordinate> getCoordinatesOfUser(Long userId, SearchCondition condition, Pageable pageable);
+
+    Coordinate updateCoordinate(CoordinateUpdateRequest request);
+
+    void deleteCoordinate(Long coordinateId);
+}
