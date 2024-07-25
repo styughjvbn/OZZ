db4898ac3713fc2dd8e94f761669f0cdf582ae20
SeoJongwon <3338761@naver.com>
2024-07-25 23:16:38 +0900

    feat: 코디 번호로 코디_옷 리스트 조회 기능 추가

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/repository/CoordinateClothesRepository.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/repository/CoordinateClothesRepository.java
@@ -4,6 +4,8 @@ import com.ssafy.ozz.clothes.coordinate.domain.Coordinate;
 import com.ssafy.ozz.clothes.coordinate.domain.CoordinateClothes;
 import com.ssafy.ozz.clothes.coordinate.domain.CoordinateClothesId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,7 @@ import java.util.List;
 @Repository
 public interface CoordinateClothesRepository extends JpaRepository<CoordinateClothes, CoordinateClothesId> {
     List<CoordinateClothes> findByCoordinate(Coordinate coordinate);
+
+    @Query("select cc from CoordinateClothes cc where cc.coordinate.coordinateId = :coordinateId")
+    List<CoordinateClothes> findByCoordinateId(@Param("coordinateId") Long coordinateId);
 }
