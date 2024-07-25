2300ccd3f59b48ec633d3c807951098a4a791ff9
SeoJongwon <3338761@naver.com>
2024-07-25 15:39:49 +0900

    feat: file 서버의 업로드 feign 통신 추가

---
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/global/fegin/file/dto/FeignFileInfo.java
@@ -0,0 +1,9 @@
+package com.ssafy.ozz.clothes.global.fegin.file.dto;
+
+public record FeignFileInfo(
+        int fileId,
+        String fileDir,
+        String fileName,
+        String fileType
+){
+}
