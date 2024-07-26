7cb8d4430bf94710daa01154cd244ecda6f99e94
SeoJongwon <3338761@naver.com>
2024-07-26 09:43:19 +0900

    feat(S11P12A804-135): 코디 수정 API 구현

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/controller/CoordinateController.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/controller/CoordinateController.java
@@ -1,8 +1,8 @@
 package com.ssafy.ozz.clothes.coordinate.controller;
 
-import com.ssafy.ozz.clothes.coordinate.domain.Coordinate;
 import com.ssafy.ozz.clothes.coordinate.dto.CoordinateCreateRequest;
 import com.ssafy.ozz.clothes.coordinate.dto.CoordinateResponse;
+import com.ssafy.ozz.clothes.coordinate.dto.CoordinateUpdateRequest;
 import com.ssafy.ozz.clothes.coordinate.dto.SearchCondition;
 import com.ssafy.ozz.clothes.coordinate.service.CoordinateService;
 import lombok.RequiredArgsConstructor;
@@ -37,5 +37,10 @@ public class CoordinateController {
         // TODO: 실제 유저 번호 받아오기
         Long userId = 1L;
         return ResponseEntity.ok(coordinateService.getCoordinatesOfUser(userId,condition,pageable).map(CoordinateResponse::new));
+    }
+
+    @PutMapping(path = "/{coordinateId}", consumes = {"multipart/form-data","application/json"})
+    public ResponseEntity<CoordinateResponse> updateCoordinate(@PathVariable final Long coordinateId, @RequestPart final MultipartFile imageFile, @RequestPart final CoordinateUpdateRequest request) {
+        return ResponseEntity.ok(new CoordinateResponse(coordinateService.updateCoordinate(coordinateId,imageFile,request)));
     }
 }
