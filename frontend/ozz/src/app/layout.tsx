91a7d84923c6c5596bbb73cd5d0324e2fe0e5710
dana019 <dana019@naver.com>
2024-07-26 14:16:36 +0900

    feat: 메인 페이지 UI 구현
    초기 메인 페이지 UI
    옷 등록 후 메인 페이지 UI
    추천 코디 Carousel
    + Header, Navbar fixed
    
    Jira: S11P12A804-78

--- frontend/ozz/src/app/layout.tsx
+++ frontend/ozz/src/app/layout.tsx
@@ -5,7 +5,7 @@ import localFont from 'next/font/local'
 const pretendard = localFont({
   src: '../../public/fonts/PretendardVariable.woff2',
   display: 'swap',
-  weight: '45 290',
+  weight: '45 920',
   variable: '--font-pretendard',
 })
 
@@ -22,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${pretendard.variable}`}>
       <body className={`${pretendard.className}`}>
-        {children}
-        <Navbar />
+        <div className="py-20">
+          {children}
+          <Navbar />
+        </div>
       </body>
     </html>
   )
