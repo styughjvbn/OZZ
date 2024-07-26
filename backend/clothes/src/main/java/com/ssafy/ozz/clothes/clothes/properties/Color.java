74528ab41de74310ae91f2322987c625ebaf0adb
SeoJongwon <3338761@naver.com>
2024-07-26 11:23:40 +0900

    fix(S11P12A804-174): 색상 코드 변경

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/clothes/properties/Color.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/clothes/properties/Color.java
@@ -4,26 +4,27 @@ import lombok.Getter;
 
 @Getter
 public enum Color {
-    BLACK("블랙", "#000000"),
-    KHAKI("카키", "#F0E68C"),
-    WHITE("화이트", "#FFFFFF"),
-    MINT("민트", "#98FF98"),
-    GREY("그레이", "#808080"),
-    BLUE("블루", "#0000FF"),
-    RED("레드", "#FF0000"),
-    NAVY("네이비", "#000080"),
-    PINK("핑크", "#FFC0CB"),
-    SKYBLUE("스카이블루", "#87CEEB"),
-    ORANGE("오렌지", "#FFA500"),
-    PURPLE("퍼플", "#800080"),
-    BEIGE("베이지", "#F5F5DC"),
-    LAVENDER("라벤더", "#E6E6FA"),
-    BROWN("브라운", "#A52A2A"),
-    WINE("와인", "#722F37"),
-    YELLOW("옐로우", "#FFFF00"),
-    NEON("네온", "#39FF14"),
-    GREEN("그린", "#008000"),
-    GOLD("골드", "#FFD700");
+
+    WHITE("흰색", "#FFFFFF"),
+    BLACK("검정", "#000000"),
+    GRAY("회색", "#E7E7E7"),
+    RED("빨강", "#FF0000"),
+    PINK("분홍", "#FEE0DE"),
+    ORANGE("주황", "#FF820E"),
+    BEIGE("베이지", "#E2C79C"),
+    YELLOW("노랑", "#FEE600"),
+    BROWN("갈색", "#844F1D"),
+    GREEN("초록", "#1A9268"),
+    KHAKI("카키", "#666B17"),
+    MINT("민트", "#6BF1D8"),
+    BLUE("파랑", "#1F4CE3"),
+    NAVY("남색", "#060350"),
+    SKY("하늘", "#C5E3FF"),
+    PURPLE("보라", "#9C53BE"),
+    LAVENDER("연보라", "#D7BEF5"),
+    WINE("와인", "#9E213F"),
+    NEON("네온", "#2FF40A"),
+    GOLD("골드", "#E6C345");
 
     private final String displayName;
     private final String colorCode;
