dfa46ad5413fe27a6fbabbf96149ec4cddd43a26
dana019 <dana019@naver.com>
2024-07-26 16:04:28 +0900

    feat: 옷장 내부 상단바 UI 구현
    옷 키워드 검색창 UI
    정렬 순서 선택창 UI
    
    Jira: S11P12A804-193

--- frontend/ozz/src/app/closet/page.tsx
+++ frontend/ozz/src/app/closet/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header'
+import SearchArea from '@/containers/closet-page/SearchArea'
 
 export default function Closet() {
   return (
     <>
       <Header title="나의 옷짱" />
+      <SearchArea />
       <h1>옷장</h1>
     </>
   )
