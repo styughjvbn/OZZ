e23a5e2a05b7e89300bd93da0ae7200f8cbb86a7
SeoJongwon <3338761@naver.com>
2024-07-28 15:13:39 +0900

    feat: fegin file client에 파일 정보 불러오기 API 추가

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/global/fegin/file/FileClient.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/global/fegin/file/FileClient.java
@@ -4,6 +4,9 @@ import com.ssafy.ozz.clothes.global.config.FeignSupportConfig;
 import com.ssafy.ozz.clothes.global.fegin.file.dto.FeignFileInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,4 +18,7 @@ public interface FileClient {
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     Optional<FeignFileInfo> uploadFile(@RequestPart("file") MultipartFile file);
+
+    @GetMapping("/{fileId}")
+    Optional<FeignFileInfo> getFile(@PathVariable("fileId") Long fileId);
 }
