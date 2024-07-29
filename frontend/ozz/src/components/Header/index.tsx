bccead7fa1ca4a59b29520b97780fdb48f37fd90
이보연 <btothey99@ewhain.net>
2024-07-29 17:48:29 +0900

    fix: merge 충돌 해결
    modify 페이지 옮김
    Header 높이 변경

--- frontend/ozz/src/components/Header/index.tsx
+++ frontend/ozz/src/components/Header/index.tsx
@@ -12,7 +12,7 @@ export default function Header({
   rightButton,
 }: HeaderProps) {
   return (
-    <header className="fixed top-0 inset-x-0 z-50 bg-secondary text-primary-400 py-5">
+    <header className="fixed top-0 inset-x-0 z-50 bg-secondary text-primary-400 pt-6 pb-5">
       <div className="container mx-auto flex justify-between items-center">
         <button>{leftButton}</button>
         <h1 className="text-3xl font-bold">{title}</h1>
