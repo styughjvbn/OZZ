429f76665d8ef183cf424ef186778f9c6403db2c
이보연 <btothey99@ewhain.net>
2024-08-01 13:42:49 +0900

    style: 옷 관련 속성 모듈화
    매핑되는 파일 따로 모듈화하여 관리
    
    Jira: S11P12A804-192

--- frontend/ozz/src/containers/closet-page/ClothingForm.tsx
+++ frontend/ozz/src/containers/closet-page/ClothingForm.tsx
@@ -16,77 +16,12 @@ import StyleModal from '@/app/@modal/style/page'
 import PatternModal from '@/app/@modal/pattern/page'
 import MemoModal from '@/app/@modal/memo/page'
 import { ClothingData } from '@/types/clothing'
+import { colorMap, textureMap, seasonMap, styleMap, fitMap } from '@/types/maps'
 
 type ClothingFormProps = {
   initialData?: ClothingData
   onSubmit: (data: FormData) => void
   submitButtonText: string
-}
-
-const colorMap: { [key: string]: string } = {
-  흰색: 'WHITE',
-  검정: 'BLACK',
-  회색: 'GRAY',
-  빨강: 'RED',
-  분홍: 'PINK',
-  주황: 'ORANGE',
-  베이지: 'BEIGE',
-  노랑: 'YELLOW',
-  갈색: 'BROWN',
-  초록: 'GREEN',
-  카키: 'KHAKI',
-  민트: 'MINT',
-  파랑: 'BLUE',
-  남색: 'NAVY',
-  하늘: 'SKY',
-  보라: 'PURPLE',
-  연보라: 'LAVENDER',
-  와인: 'WINE',
-  네온: 'NEON',
-  골드: 'GOLD',
-}
-
-const textureMap: { [key: string]: string } = {
-  무지: 'PLAIN',
-  줄무늬: 'STRIPED',
-  지그재그: 'ZIGZAG',
-  호피: 'LEOPARD',
-  지브라: 'ZEBRA',
-  아가일: 'ARGYLE',
-  도트: 'DOT',
-  페이즐리: 'PAISLEY',
-  카모플라쥬: 'CAMOUFLAGE',
-  플로럴: 'FLORAL',
-  레터링: 'LETTERING',
-  그래픽: 'GRAPHIC',
-  해골: 'SKULL',
-  타이다이: 'TIE_DYE',
-  깅엄: 'GINGHAM',
-  그라데이션: 'GRADATION',
-  체크: 'CHECK',
-  하운즈투스: 'HOUNDSTOOTH',
-}
-
-const seasonMap: { [key: string]: string } = {
-  봄: 'SPRING',
-  여름: 'SUMMER',
-  가을: 'AUTUMN',
-  겨울: 'WINTER',
-}
-
-const styleMap: { [key: string]: string } = {
-  캐주얼: 'CASUAL',
-  포멀: 'FORMAL',
-  스트릿: 'STREET',
-  스포츠: 'SPORTS',
-  빈티지: 'VINTAGE',
-}
-
-const fitMap: { [key: string]: string } = {
-  오버핏: 'OVERSIZE',
-  세이오버핏: 'SEMIOVER',
-  레귤러핏: 'REGULAR',
-  슬림핏: 'SLIM',
 }
 
 export default function ClothingForm({
