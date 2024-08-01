ed90460378d80eb830455ee33ef3a714984c8b64
이보연 <btothey99@ewhain.net>
2024-08-01 09:49:40 +0900

    refactor: 가상피팅 레이아웃 분리
    헤더를 layout.tsx로 분리
    
    Jira: S11P12A804-204

--- frontend/ozz/src/app/fitting/page.tsx
+++ frontend/ozz/src/app/fitting/page.tsx
@@ -1,10 +1,8 @@
-import Header from '@/components/Header'
 import FittingContainer from '@/containers/fitting-page'
 
 export default function Fitting() {
   return (
     <>
-      <Header title="가상 피팅" />
       <FittingContainer />
     </>
   )
