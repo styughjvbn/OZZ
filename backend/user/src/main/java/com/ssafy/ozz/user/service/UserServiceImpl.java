c7aea29836c4bd2f10663b471d1b42964f5ec0f5
doongyeop <leedy977@gmail.com>
2024-08-02 16:39:42 +0900

    chore: @Authwired 삭제

--- backend/user/src/main/java/com/ssafy/ozz/user/service/UserServiceImpl.java
+++ backend/user/src/main/java/com/ssafy/ozz/user/service/UserServiceImpl.java
@@ -12,8 +12,7 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
