fc027566bb0d3fc06fb2aa8fec6af26ba5e1cccf
SeoJongwon <3338761@naver.com>
2024-08-04 18:51:26 +0900

    feat : openai 연동

--- backend/ai/app/core/config.py
+++ backend/ai/app/core/config.py
@@ -1,5 +1,9 @@
 import openai
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 class Settings:
     OPENAI_API_KEY: str = os.getenv('OPENAI_API_KEY')
--- backend/ai/app/main.py
+++ backend/ai/app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import attributes
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # API 라우터 설정
 app.include_router(attributes.router, prefix="/api/v1")
