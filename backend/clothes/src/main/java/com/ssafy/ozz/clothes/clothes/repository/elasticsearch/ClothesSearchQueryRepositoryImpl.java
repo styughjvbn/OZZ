72f7bca7f029d344eaa22bed9c8302fa16862ada
SeoJongwon <3338761@naver.com>
2024-08-04 23:11:41 +0900

    refactor: 안쓰는 애노테이션 제거

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/clothes/repository/elasticsearch/ClothesSearchQueryRepositoryImpl.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/clothes/repository/elasticsearch/ClothesSearchQueryRepositoryImpl.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-@Repository
 @RequiredArgsConstructor
 public class ClothesSearchQueryRepositoryImpl implements ClothesSearchQueryRepository {
 
