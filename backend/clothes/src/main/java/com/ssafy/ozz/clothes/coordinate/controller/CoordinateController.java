313807886270a03c95a6d7bcecc8d284c515946f
SeoJongwon <3338761@naver.com>
2024-08-06 10:22:16 +0900

    docs: 코디 api에 swagger 설정 추가

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/controller/CoordinateController.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/coordinate/controller/CoordinateController.java
@@ -2,7 +2,9 @@ package com.ssafy.ozz.clothes.coordinate.controller;
 
 import com.ssafy.ozz.clothes.coordinate.dto.*;
 import com.ssafy.ozz.clothes.coordinate.service.CoordinateService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -16,39 +18,44 @@ import static com.ssafy.ozz.clothes.global.config.HeaderConfig.X_USER_ID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/coordinates")
+@Tag(name = "Coordinate API", description = "코디 관리 API")
 public class CoordinateController {
     private final CoordinateService coordinateService;
 
     @PostMapping(consumes = {"multipart/form-data","application/json"})
+    @Operation(summary = "새 코디 추가", description = "데이터베이스에 새 코디를 추가합니다.")
     public ResponseEntity<Long> createCoordinate(@Parameter(hidden = true) @RequestHeader(X_USER_ID) Long userId, @RequestPart final MultipartFile imageFile, @RequestPart final CoordinateCreateRequest request) {
-//        Long userId = 1L;
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(coordinateService.createCoordinate(userId,imageFile,request).coordinateId());
     }
 
     @GetMapping("/{coordinateId}")
+    @Operation(summary = "코디 상세 조회", description = "ID를 통해 특정 코디 세부 정보를 조회합니다.")
     public ResponseEntity<CoordinateResponse> getCoordinate(@PathVariable final Long coordinateId) {
         return ResponseEntity.ok(coordinateService.getCoordinate(coordinateId));
     }
 
-    @GetMapping
+    @GetMapping("/users")
+    @Operation(summary = "사용자의 코디 조회", description = "특정 사용자의 코디 목록을 슬라이스 형태로 조회합니다.")
     public ResponseEntity<Slice<CoordinateResponse>> getCoordinateList(@Parameter(hidden = true) @RequestHeader(X_USER_ID) Long userId, @ModelAttribute CoordinateSearchCondition condition, Pageable pageable) {
-//        Long userId = 1L;
         return ResponseEntity.ok(coordinateService.getCoordinatesOfUser(userId,condition,pageable));
     }
 
     @GetMapping("/search")
+    @Operation(summary = "키워드로 코디 검색", description = "검색된 코디 목록을 슬라이스 형태로 조회합니다.")
     public ResponseEntity<Slice<CoordinateBasicResponse>> searchCoordinateList(@ModelAttribute CoordinateSearchCondition condition, Pageable pageable) {
         return ResponseEntity.ok(coordinateService.searchCoordinates(condition,pageable));
     }
 
 
     @PutMapping(path = "/{coordinateId}", consumes = {"multipart/form-data","application/json"})
+    @Operation(summary = "코디 정보 수정", description = "ID를 통해 특정 코디의 세부 정보를 수정합니다.")
     public ResponseEntity<CoordinateResponse> updateCoordinate(@PathVariable final Long coordinateId, @RequestPart final MultipartFile imageFile, @RequestPart final CoordinateUpdateRequest request) {
         return ResponseEntity.ok(coordinateService.updateCoordinate(coordinateId,imageFile,request));
     }
 
     @DeleteMapping("/{coordinateId}")
+    @Operation(summary = "코디 삭제", description = "ID를 통해 특정 코디를 삭제합니다.")
     public ResponseEntity<Void> deleteCoordinate(@PathVariable final Long coordinateId) {
         coordinateService.deleteCoordinate(coordinateId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
