6b05e93a122c5a2aef56ceb505b06b3844e9526b
doongyeop <leedy977@gmail.com>
2024-08-06 11:06:25 +0900

    fix: 파라미터 수정
    X-USER-ID
    
    Jira: S11P12A804-292

--- backend/user/src/main/java/com/ssafy/ozz/user/controller/UserController.java
+++ backend/user/src/main/java/com/ssafy/ozz/user/controller/UserController.java
@@ -8,16 +8,14 @@ import com.ssafy.ozz.user.global.file.dto.FeignFileInfo;
 import com.ssafy.ozz.user.global.file.exception.FileUploadException;
 import com.ssafy.ozz.user.service.UserService;
 import com.ssafy.ozz.user.util.JWTUtil;
-import io.jsonwebtoken.Jwts;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Optional;
 
@@ -34,9 +32,7 @@ public class UserController {
 
     @GetMapping("/")
     @Operation(summary = "토큰으로 유저정보를 조회")
-    public ResponseEntity<?> getUserInfo(@RequestHeader("Authorization") String token) {
-        Long userId = getUserIdFromToken(token);
-
+    public ResponseEntity<?> getUserInfo(@Parameter(hidden = true) @RequestHeader("X_USER_ID") Long userId) {
         Optional<User> userOptional = userService.getUserById(userId);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
@@ -48,9 +44,7 @@ public class UserController {
 
     @PutMapping("/")
     @Operation(summary = "토큰으로 유저정보 수정")
-    public ResponseEntity<?> updateUser(@RequestHeader("Authorization") String token, @RequestBody UserUpdateRequest updates) {
-        Long userId = getUserIdFromToken(token);
-
+    public ResponseEntity<?> updateUser(@Parameter(hidden = true) @RequestHeader("X_USER_ID") Long userId, @RequestBody UserUpdateRequest updates) {
         Optional<User> userOptional = userService.getUserById(userId);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
@@ -60,14 +54,9 @@ public class UserController {
             if (nickname != null) {
                 userBuilder.nickname(nickname);
             }
-            String birthstr = String.valueOf(updates.birth());
-            if (birthstr != null) {
-                try {
-                    Date birth = new SimpleDateFormat("yyyy-MM-dd").parse(birthstr);
-                    userBuilder.birth(birth);
-                } catch (ParseException e) {
-                    return ResponseEntity.badRequest().body("yyyy-MM-dd로 입력해주세요.");
-                }
+            Date birth = updates.birth();
+            if (birth != null) {
+                userBuilder.birth(birth);
             }
 
             User updatedUser = userService.updateUser(userId, userBuilder.build());
@@ -82,25 +71,16 @@ public class UserController {
         }
     }
 
-    private Long getUserIdFromToken(String token) {
-        // 토큰에서 userId를 추출하는 로직
-        // 예시: JWT 토큰에서 클레임으로 userId 추출
-        return Long.parseLong(Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject());
-    }
-}
-
     @PatchMapping("/profile")
     @Operation(summary = "유저 프로필 변경")
-    public ResponseEntity<?> uploadProfileImage(@RequestHeader("Authorization") String token, @RequestParam("file") MultipartFile file) {
-        Long userId = getUserIdFromToken(token);
-
+    public ResponseEntity<?> uploadProfileImage(@Parameter(hidden = true) @RequestHeader("X_USER_ID") Long userId, @RequestParam("file") MultipartFile file) {
         Optional<User> userOptional = userService.getUserById(userId);
         if (userOptional.isPresent()) {
             try {
                 FeignFileInfo fileInfo = fileClient.uploadFile(file).orElseThrow(FileUploadException::new);
                 User user = userOptional.get();
                 User updatedUser = user.toBuilder()
-                        .profileFileId(fileInfo.fileId())
+                        .profileFileId(fileInfo.getFileId())
                         .build();
                 userService.updateUser(userId, updatedUser);
                 return ResponseEntity.ok(fileInfo);
@@ -114,41 +94,18 @@ public class UserController {
 
     @DeleteMapping("/")
     @Operation(summary = "회원 탈퇴")
-    public ResponseEntity<?> deleteUser(@RequestHeader("Authorization") String token) {
-        Long userId = getUserIdFromToken(token);
+    public ResponseEntity<?> deleteUser(@Parameter(hidden = true) @RequestHeader("X_USER_ID") Long userId) {
 
         Optional<User> userOptional = userService.getUserById(userId);
         if (userOptional.isPresent()) {
             userService.deleteUser(userId);
             // 토큰도 삭제
-            String existingTokenKey = findExistingRefreshTokenKey(userId);
-            if (existingTokenKey != null) {
-                redisTemplate.delete(existingTokenKey);
-            }
+            authClient.deleteRefreshTokenOfUser(userId);
             return ResponseEntity.status(204).body("no content");
         } else {
             return ResponseEntity.status(404).body("User not found");
         }
     }
-    
-//    // 걍 updateUser(45줄) 이거쓰면 될 듯
-//    @PutMapping("/nickname")
-//    @Operation(summary = "최초 로그인 시 닉네임 입력")
-//    public ResponseEntity<?> registNickName(@RequestHeader("Authorization") String token, @RequestParam(value = "nickname") String nickname) {
-//        Long userId = getUserIdFromToken(token);
-//
-//        Optional<User> userOptional = userService.getUserById(userId);
-//        if (userOptional.isPresent()) {
-//            User user = userOptional.get();
-//            User updatedUser = user.toBuilder()
-//                    .nickname(nickname)
-//                    .build();
-//            userService.updateUser(userId, updatedUser);
-//            return ResponseEntity.ok("닉네임이 등록되었습니다.");
-//        } else {
-//            return ResponseEntity.status(404).body("User not found");
-//        }
-//    }
 
     @GetMapping("/check")
     @Operation(summary = "닉네임 중복 조회")
@@ -157,12 +114,6 @@ public class UserController {
             throw new ResponseStatusException(BAD_REQUEST, "이미 사용중인 닉네임입니다.");
         }
         return ResponseEntity.ok("사용 가능한 닉네임입니다.");
-    }
-
-    // 토큰에서 userId 추출해서 Long 타입으로 반환
-    private Long getUserIdFromToken(String token) {
-        String userIdStr = jwtUtil.getUserId(token.replace("Bearer ", ""));
-        return Long.parseLong(userIdStr);
     }
 
 }
