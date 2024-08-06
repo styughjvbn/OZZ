ee6587ca8cc0ae004c098a58f20d02b981e9e715
doongyeop <leedy977@gmail.com>
2024-08-06 12:31:34 +0900

    fix: 미사용 import 제거

--- backend/board/src/main/java/com/ssafy/ozz/board/controller/BoardController.java
+++ backend/board/src/main/java/com/ssafy/ozz/board/controller/BoardController.java
@@ -13,7 +13,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/boards")
--- backend/board/src/main/java/com/ssafy/ozz/board/service/BoardService.java
+++ backend/board/src/main/java/com/ssafy/ozz/board/service/BoardService.java
@@ -7,7 +7,6 @@ import com.ssafy.ozz.board.dto.response.BoardResponse;
 import com.ssafy.ozz.board.dto.response.BoardWithFileResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 
