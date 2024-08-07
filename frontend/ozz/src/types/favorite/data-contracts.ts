09cde266249b4d30405d3547773e45482a3ece96
dana019 <dana019@naver.com>
2024-08-07 13:48:51 +0900

    feat: swagger-typescript-api 연결
    Jira: S11P12A804-316

---
+++ frontend/ozz/src/types/favorite/data-contracts.ts
@@ -0,0 +1,96 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export interface FavoriteGroupCreateRequest {
+  name?: string
+}
+
+export interface FavoriteGroupBasicResponse {
+  /** @format int64 */
+  favoriteGroupId?: number
+  name?: string
+}
+
+export interface CoordinateBasicResponse {
+  /** @format int64 */
+  coordinateId?: number
+  name?: string
+  styleList?: (
+    | 'CASUAL'
+    | 'CLASSIC'
+    | 'MANNISH'
+    | 'FEMININE'
+    | 'HIPPIE'
+    | 'MODERN'
+    | 'COUNTRY'
+    | 'GENDERLESS'
+    | 'SPORTY'
+    | 'RETRO'
+    | 'MILITARY'
+    | 'PREPPY'
+    | 'TOMBOY'
+    | 'ROMANTIC'
+    | 'WESTERN'
+    | 'SOPHISTICATED'
+    | 'COTTAGER'
+    | 'RESORT'
+    | 'KITSCH'
+    | 'KIDULT'
+    | 'STREET'
+    | 'SEXY'
+    | 'ORIENTAL'
+    | 'AVANT_GARDE'
+    | 'HIPHOP'
+    | 'PUNK'
+  )[]
+  /** @format date-time */
+  createdDate?: string
+  imageFile?: FileInfo
+}
+
+export interface FavoriteResponse {
+  /** @format int64 */
+  favoriteId?: number
+  coordinate?: CoordinateBasicResponse
+}
+
+export interface FileInfo {
+  /** @format int64 */
+  fileId?: number
+  filePath?: string
+  fileName?: string
+  fileType?: string
+}
+
+export interface FavoriteGroupImageResponse {
+  /** @format int64 */
+  favoriteGroupId?: number
+  name?: string
+  imageFileList?: FileInfo[]
+}
+
+export interface FavoriteListDeleteRequest {
+  coordinateIdList?: number[]
+}
+
+export type CreateFavoriteGroupData = FavoriteGroupBasicResponse
+
+export type AddFavoriteData = FavoriteResponse
+
+export type DeleteFavorite1Data = any
+
+export type GetFavoritesByGroupData = FavoriteResponse[]
+
+export type DeleteFavoriteGroupData = any
+
+export type GetFavoritesGroupListOfUsersData = FavoriteGroupImageResponse[]
+
+export type DeleteFavoriteData = any
---
+++ frontend/ozz/src/types/user/Api.ts
@@ -0,0 +1,124 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+import {
+  CheckNicknameData,
+  DeleteUserData,
+  GetUserInfoData,
+  UpdateUserData,
+  UploadProfileImageData,
+  UploadProfileImagePayload,
+  UserUpdateRequest,
+} from './data-contracts'
+import { ContentType, HttpClient, RequestParams } from './http-client'
+
+export class Api<
+  SecurityDataType = unknown,
+> extends HttpClient<SecurityDataType> {
+  /**
+   * No description
+   *
+   * @tags user-controller
+   * @name GetUserInfo
+   * @summary 토큰으로 유저정보를 조회
+   * @request GET:/api/users/
+   * @secure
+   * @response `200` `GetUserInfoData` OK
+   */
+  getUserInfo = (params: RequestParams = {}) =>
+    this.request<GetUserInfoData, any>({
+      path: `/api/users/`,
+      method: 'GET',
+      secure: true,
+      ...params,
+    })
+  /**
+   * No description
+   *
+   * @tags user-controller
+   * @name UpdateUser
+   * @summary 토큰으로 유저정보 수정
+   * @request PUT:/api/users/
+   * @secure
+   * @response `200` `UpdateUserData` OK
+   */
+  updateUser = (data: UserUpdateRequest, params: RequestParams = {}) =>
+    this.request<UpdateUserData, any>({
+      path: `/api/users/`,
+      method: 'PUT',
+      body: data,
+      secure: true,
+      type: ContentType.Json,
+      ...params,
+    })
+  /**
+   * No description
+   *
+   * @tags user-controller
+   * @name DeleteUser
+   * @summary 회원 탈퇴
+   * @request DELETE:/api/users/
+   * @secure
+   * @response `200` `DeleteUserData` OK
+   */
+  deleteUser = (params: RequestParams = {}) =>
+    this.request<DeleteUserData, any>({
+      path: `/api/users/`,
+      method: 'DELETE',
+      secure: true,
+      ...params,
+    })
+  /**
+   * No description
+   *
+   * @tags user-controller
+   * @name UploadProfileImage
+   * @summary 유저 프로필 변경
+   * @request PATCH:/api/users/profile
+   * @secure
+   * @response `200` `UploadProfileImageData` OK
+   */
+  uploadProfileImage = (
+    data: UploadProfileImagePayload,
+    params: RequestParams = {},
+  ) =>
+    this.request<UploadProfileImageData, any>({
+      path: `/api/users/profile`,
+      method: 'PATCH',
+      body: data,
+      secure: true,
+      type: ContentType.FormData,
+      ...params,
+    })
+  /**
+   * No description
+   *
+   * @tags user-controller
+   * @name CheckNickname
+   * @summary 닉네임 중복 조회
+   * @request GET:/api/users/check
+   * @secure
+   * @response `200` `CheckNicknameData` OK
+   */
+  checkNickname = (
+    query: {
+      nickname: string
+    },
+    params: RequestParams = {},
+  ) =>
+    this.request<CheckNicknameData, any>({
+      path: `/api/users/check`,
+      method: 'GET',
+      query: query,
+      secure: true,
+      ...params,
+    })
+}
