c98c17c6997e52dbef98b157d4e7935b181e4c9f
이보연 <btothey99@ewhain.net>
2024-08-07 09:45:28 +0900

    style: 폴더 구조 변경
    코디샷 페이지 따로 구성
    
    Jira: S11P12A804-216

--- frontend/ozz/src/app/community/page.tsx
+++ frontend/ozz/src/app/community/page.tsx
@@ -1,11 +1,9 @@
 import CommunityPreview from '@/containers/community/CommunityPreview'
-import CoordiShotContainer from '@/containers/coordishot-page'
 
 export default function Community() {
   return (
     <>
       <CommunityPreview />
-      <CoordiShotContainer />
     </>
   )
 }
