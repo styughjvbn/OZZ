3ef0707abc5a2099e8249bdd7f8bd4e7860d943f
SeoJongwon <3338761@naver.com>
2024-08-08 10:09:53 +0900

    feat: 예외 처리 클래스 추가

---
+++ backend/favorite/src/main/java/com/ssafy/ozz/favorite/exception/FavoriteExceptionAdvice.java
@@ -0,0 +1,17 @@
+package com.ssafy.ozz.favorite.exception;
+
+import com.ssafy.ozz.library.error.ErrorCode;
+import com.ssafy.ozz.library.error.ServiceException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class FavoriteExceptionAdvice {
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<String> handleSerialException(final ServiceException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(errorCode.getMessage());
+    }
+}
