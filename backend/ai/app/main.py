53866714b845ae1801954830aa166b1760654aa1
SeoJongwon <3338761@naver.com>
2024-08-08 16:25:51 +0900

    fix : 구매내역 정규화 라우터 추가

--- backend/ai/app/main.py
+++ backend/ai/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import attributes, tokenize
+from app.api.v1.endpoints import attributes, tokenize, purchaseHistory
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
@@ -8,7 +8,10 @@ origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
-    "http://localhost:8080",
+    "http://localhost:5500",
+    "http://localhost:8000",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:8000"
 ]
 
 app.add_middleware(
@@ -21,3 +24,4 @@ app.add_middleware(
 # API 라우터 설정
 app.include_router(attributes.router, prefix="/api/v1")
 app.include_router(tokenize.router)
+app.include_router(purchaseHistory.router)
