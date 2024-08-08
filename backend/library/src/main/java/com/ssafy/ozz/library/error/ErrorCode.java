3cf7ff98950166969ecf6ca09c44c4b694de295e
SeoJongwon <3338761@naver.com>
2024-08-08 10:17:05 +0900

    fix: 에러 코드 수정

--- backend/library/src/main/java/com/ssafy/ozz/library/error/ErrorCode.java
+++ backend/library/src/main/java/com/ssafy/ozz/library/error/ErrorCode.java
@@ -7,14 +7,14 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "서버 오류가 발생했습니다."),
-    CLOTHES_NOT_FOUND(400, "옷을 찾을 수 없습니다."),
-    CATEGORY_NOT_FOUND(400, "카테고리를 찾을 수 없습니다."),
-    COORDINATE_NOT_FOUND(400, "코디를 찾을 수 없습니다."),
-    FILE_NOT_FOUND(400, "파일을 찾을 수 없습니다."),
-    BOARD_NOT_FOUND(400, "게시글을 찾을 수 없습니다."),
+    CLOTHES_NOT_FOUND(404, "옷을 찾을 수 없습니다."),
+    CATEGORY_NOT_FOUND(404, "카테고리를 찾을 수 없습니다."),
+    COORDINATE_NOT_FOUND(404, "코디를 찾을 수 없습니다."),
+    FILE_NOT_FOUND(404, "파일을 찾을 수 없습니다."),
+    BOARD_NOT_FOUND(404, "게시글을 찾을 수 없습니다."),
     FAVORITE_DUPLICATED(400, "즐겨찾기 그룹에 해당 코디가 존재합니다."),
-    FAVORITE_NOT_FOUND(400, "즐겨찾기를 찾을 수 없습니다."),
-    FAVORITE_GROUP_NOT_FOUND(400, "즐겨찾기 그룹을 찾을 수 없습니다.");
+    FAVORITE_NOT_FOUND(404, "즐겨찾기를 찾을 수 없습니다."),
+    FAVORITE_GROUP_NOT_FOUND(404, "즐겨찾기 그룹을 찾을 수 없습니다.");
 
     private final int httpStatus;
     private final String message;
