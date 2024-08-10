580c7916f040b2fea75cc1425f3f229b83bcbba1
SeoJongwon <3338761@naver.com>
2024-08-10 21:53:37 +0900

    fix: response dto null 처리

--- backend/clothes/src/main/java/com/ssafy/ozz/clothes/category/dto/CategoryHighBasicResponse.java
+++ backend/clothes/src/main/java/com/ssafy/ozz/clothes/category/dto/CategoryHighBasicResponse.java
@@ -1,11 +1,14 @@
 package com.ssafy.ozz.clothes.category.dto;
 
 import com.ssafy.ozz.clothes.category.domain.CategoryHigh;
+import com.ssafy.ozz.clothes.category.domain.CategoryLow;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
 import java.util.List;
 
 @Schema(description = "상위 카테고리 기본 정보 DTO")
+@Builder
 public record CategoryHighBasicResponse(
     Byte categoryHighId,
     String name
@@ -15,5 +18,17 @@ public record CategoryHighBasicResponse(
             categoryHigh.getCategoryHighId(),
             categoryHigh.getName()
         );
+    }
+    public static CategoryHighBasicResponse of(CategoryHigh categoryHigh) {
+        return categoryHigh != null ? CategoryHighBasicResponse.builder()
+                .categoryHighId(categoryHigh.getCategoryHighId())
+                .name(categoryHigh.getName())
+                .build() : null;
+    }
+    public static CategoryHighBasicResponse of(CategoryLow categoryLow) {
+        return (categoryLow != null && categoryLow.getCategoryHigh() != null) ? CategoryHighBasicResponse.builder()
+                .categoryHighId(categoryLow.getCategoryHigh().getCategoryHighId())
+                .name(categoryLow.getCategoryHigh().getName())
+                .build() : null;
     }
 }
