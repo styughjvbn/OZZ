d6f4a8e874639367426f561c5eee816b103f1bc5
이보연 <btothey99@ewhain.net>
2024-08-10 22:30:50 +0900

    feat: 디버깅 추가
    디버깅용 console 추가
    
    Jira: S11P12A804-334

--- frontend/ozz/src/app/api/authTokens/route.ts
+++ frontend/ozz/src/app/api/authTokens/route.ts
@@ -5,10 +5,12 @@ import { NextRequest, NextResponse } from 'next/server'
 // import { cookies } from 'next/headers'
 
 export async function GET(req: NextRequest) {
+  console.log('authTokens GET 시작')
   // const cookieStore = cookies()
   // const accessToken = cookieStore.get('access')?.value
   // const refreshToken = cookieStore.get('refresh')?.value
   const cookieHeader = req.headers.get('cookie')
+  console.log('cookieHeader? ', cookieHeader)
   if (cookieHeader) {
     const cookies = cookieHeader.split('; ').reduce(
       (acc, cookie) => {
@@ -19,11 +21,11 @@ export async function GET(req: NextRequest) {
       {} as Record<string, string>,
     )
 
-    // console.log('쿠키에서 읽어온 accessToken:', accessToken)
-    // console.log('쿠키에서 읽어온 refreshToken:', refreshToken)
-
     const accessToken = cookies.access
     const refreshToken = cookies.refresh
+
+    console.log('쿠키에서 읽어온 accessToken:', accessToken)
+    console.log('쿠키에서 읽어온 refreshToken:', refreshToken)
 
     if (accessToken && refreshToken) {
       return NextResponse.json({ accessToken, refreshToken })
