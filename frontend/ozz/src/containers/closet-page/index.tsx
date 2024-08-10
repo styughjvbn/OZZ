ed878566682009fe1e561a5cee8ed9224b08440b
dana019 <dana019@naver.com>
2024-08-10 17:30:01 +0900

    feat: 옷 목록 키워드 검색 및 정렬 기능 구현
    Jira: S11P12A804-351

--- frontend/ozz/src/containers/closet-page/index.tsx
+++ frontend/ozz/src/containers/closet-page/index.tsx
@@ -7,9 +7,11 @@ import SearchArea from '@/containers/closet-page/SearchArea'
 import EmptyCloset from '@/containers/closet-page/EmptyCloset/page'
 import { fetchImage, fetchUserClothes } from '@/services/clothingApi'
 import Loading from '@/app/closet/loading'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { ImSpinner8 } from 'react-icons/im'
 import { categoryMap, categoryNameToLowIdMap } from '@/types/clothing'
+
+type OrderValue = 'createdDate' | 'purchaseDate'
 
 const queryKeys = {
   userClothes: 'userClothes',
@@ -19,6 +21,9 @@ export default function ClosetPageContainer() {
   const { isSidebarOpen, selectedCategory, selectedSubcategory } =
     useCategorySidebar()
   const observerElem = useRef(null)
+
+  const [order, setOrder] = useState<OrderValue>('createdDate')
+  const [searchKeyword, setSearchKeyword] = useState('')
 
   // 카테고리 이름을 ID로 변환
   const categoryHighId =
@@ -38,22 +43,32 @@ export default function ClosetPageContainer() {
     isFetchingNextPage,
     isLoading,
     isError,
+    refetch,
   } = useInfiniteQuery({
     queryKey: [queryKeys.userClothes, selectedCategory, selectedSubcategory],
     queryFn: ({ pageParam = 0 }) => {
       return fetchUserClothes(
-        { page: pageParam, size: 20 },
+        { page: pageParam, size: 20, sort: [`${order},desc`] },
         {
           categoryHighId,
           categoryLowId,
+          keyword: searchKeyword,
         },
       )
     },
     getNextPageParam: (lastPage) =>
-      // If last page's 'last' field is true, there are no more pages
       lastPage.last ? undefined : lastPage.number + 1,
     initialPageParam: 0,
   })
+
+  // Submit 핸들러 정의
+  const handleSubmit = () => {
+    refetch().then()
+  }
+
+  useEffect(() => {
+    refetch().then()
+  }, [order, refetch])
 
   // Flatten the pages into a single list
   const clothingList = data?.pages.flatMap((page) => page.content) || []
@@ -105,7 +120,13 @@ export default function ClosetPageContainer() {
         <EmptyCloset />
       ) : (
         <>
-          <SearchArea />
+          <SearchArea
+            searchKeyword={searchKeyword}
+            setSearchKeyword={setSearchKeyword}
+            order={order}
+            setOrder={setOrder}
+            onSubmit={handleSubmit}
+          />
           <ClothesList clothingList={clothingWithImages} isSelectable={false} />
           <div ref={observerElem}>
             {isFetchingNextPage && (
