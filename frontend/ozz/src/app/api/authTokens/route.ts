d8928725709997f2f1804afe0b26bf36056e780d
이보연 <btothey99@ewhain.net>
2024-08-11 14:54:02 +0900

    feat: 쿠키 설정
    모듈 충돌이 있어서 수정
    
    Jira: S11P12A804-334

--- frontend/ozz/src/app/api/authTokens/route.ts
+++ frontend/ozz/src/app/api/authTokens/route.ts
@@ -1,15 +1,12 @@
-'use server'
-
 /* eslint-disable import/prefer-default-export */
 
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse } from 'next/server'
 import cookie from 'cookie'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // 쿠키 헤더에서 쿠키를 파싱합니다.
-  const cookies = cookie.parse(req.headers.cookie || '')
+export async function GET(request: Request) {
+  const cookieHeader = request.headers.get('cookie') || ''
+  const cookies = cookie.parse(cookieHeader)
 
-  // 특정 쿠키를 가져옵니다.
   const accessToken = cookies.access
   const refreshToken = cookies.refresh
 
@@ -17,8 +14,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log('Refresh Token:', refreshToken)
 
   if (accessToken && refreshToken) {
-    return res.status(200).json({ accessToken, refreshToken })
-  } else {
-    return res.status(404).json({ message: 'Tokens not found' })
+    return NextResponse.json({ accessToken, refreshToken })
   }
+  return NextResponse.json({ message: 'Tokens not found' }, { status: 404 })
 }
