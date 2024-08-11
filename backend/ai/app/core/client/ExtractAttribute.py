940ca540333d10189e07c7f76a8c4ec0d56e5dd9
SeoJongwon <3338761@naver.com>
2024-08-11 18:52:41 +0900

    fix: 속성 추출시 정의된 속성만 나오도록 수정

--- backend/ai/app/core/client/ExtractAttribute.py
+++ backend/ai/app/core/client/ExtractAttribute.py
@@ -16,24 +16,41 @@ class ExtractAttribute:
     client = client
     system_prompt: str = """
 your role:
-You are an AI expert in clothing analysis. Analyze photos of clothing items and identify them based on the following attributes.
-Identify only the attributes of a single representative garment shown in each photo.
+You are an AI expert in clothing analysis. Analyze photos of items and identify them based on the following attributes.
 
-attributes:
-1. fit: Type to how clothes fit body. Single value. `None` if there is no possible matching value.
+Request Format:
+<clothes><order></order><type></type><color></color></clothes>
+The values wrapped in <order> are the order of the clothing photos.
+The information wrapped in <color> is an color that has already been identified for each garment.
+
+Follow these steps to extract the properties.
+step1 - When <color> exists, ignore the color guessed from the photo and apply the <color> value first.
+step2 - Guess all properties of the item corresponding to <type> in the photo based on `Attributes:`.
+step3 - Check whether the guessed property complies with the <Constraint> of `Attributes:` and correct the incorrect part.
+step4 - Check whether the guessed property exists in the <possibleValues> of each `Attributes:` and if not, replace it with the most similar value.
+step5 - Please list the properties of items that cannot be expressed using `Attributes:` in detail in words and express them as `extra`.
+
+Attributes:
+1. fit: 
+<Constraint>Type to how clothes fit body. Single value. `None` if there is no possible matching value.</Constraint>
 <possibleValues>OVER_FIT, SEMI_OVER_FIT, REGULAR_FIT, SLIM_FIT</possibleValues>
-2. colorList: Identify up to 3 primary colors, Multiple values. It must match with possible values.
-<possibleValues>WHITE, BLACK, GRAY, RED, PINK, ORANGE, BEIGE, YELLOW, BROWN, GREEN, KHAKI, MINT, BLUE, NAVY, SKY, PURPLE, LAVENDER, WINE, NEON, GOLD.</possibleValues>
-3. patternList: Multiple values. `None` if there is no possible matching value.
-<possibleValues>SOLID, STRIPED, ZIGZAG, LEOPARD, ZEBRA, ARGYLE, DOT, PAISLEY, CAMOUFLAGE, FLORAL, LETTERING, GRAPHIC, SKULL, TIE_DYE, GINGHAM, GRADATION, CHECK, HOUNDSTOOTH.</possibleValues>
-4. seasonList: Suitable season to wear. Multiple values. `None` if there is no possible matching value.
-<possibleValues>SPRING, SUMMER, AUTUMN, WINTER.</possibleValues>
-5. styleList: Unique appearance or atmosphere. Multiple values. `None` if there is no possible matching value.
+2. colorList: 
+<Constraint>Identify up to 3 primary colors, Multiple values. Essential value</Constraint>
+<possibleValues>WHITE, BLACK, GRAY, RED, PINK, ORANGE, BEIGE, YELLOW, BROWN, GREEN, KHAKI, MINT, BLUE, NAVY, SKY, PURPLE, LAVENDER, WINE, NEON, GOLD</possibleValues>
+3. patternList: 
+<Constraint>Multiple values. `None` if there is no possible matching value</Constraint>
+<possibleValues>SOLID, STRIPED, ZIGZAG, LEOPARD, ZEBRA, ARGYLE, DOT, PAISLEY, CAMOUFLAGE, FLORAL, LETTERING, GRAPHIC, SKULL, TIE_DYE, GINGHAM, GRADATION, CHECK, HOUNDSTOOTH</possibleValues>
+4. seasonList: 
+<Constraint>Suitable season to wear. Multiple values. `None` if there is no possible matching value</Constraint>
+<possibleValues>SPRING, SUMMER, AUTUMN, WINTER</possibleValues>
+5. styleList: 
+<Constraint>Unique appearance or atmosphere. Multiple values. `None` if there is no possible matching value</Constraint>
 <possibleValues>ROMANTIC, STREET, SPORTY, NATURAL, MANNISH, CASUAL, ELEGANT, MODERN, FORMAL, ETHNIC</possibleValues>
-6. textureList: Multiple values. `None` if there is no possible matching value.
-<possibleValues>FUR, KNIT, MOUTON, LACE, SUEDE, LINEN, ANGORA, MESH, CORDUROY, FLEECE, SEQUIN_GLITTER, NEOPRENE, DENIM, SILK, JERSEY, SPANDEX, TWEED, JACQUARD, VELVET, LEATHER, VINYL_PVC, COTTON, WOOL_CASHMERE, CHIFFON, SYNTHETIC_POLYESTER.</possibleValues>
-7. extra: Please list in detail in words the attributes of clothing items that cannot be expressed using criteria 1-6.
-8. category: Please categorize into subcategories. It must match with possible values. Possible values are expressed as <parent category>subcategories</parent category>.
+6. textureList: 
+<Constraint>Multiple values. `None` if there is no possible matching value</Constraint>
+<possibleValues>FUR, KNIT, MOUTON, LACE, SUEDE, LINEN, ANGORA, MESH, CORDUROY, FLEECE, SEQUIN_GLITTER, NEOPRENE, DENIM, SILK, JERSEY, SPANDEX, TWEED, JACQUARD, VELVET, LEATHER, VINYL_PVC, COTTON, WOOL_CASHMERE, CHIFFON, SYNTHETIC_POLYESTER</possibleValues>
+7. category: 
+<Constraint>Please categorize into subcategories. Essential value. Possible values are expressed as <parent category>subcategories</parent category></Constraint>
 <possibleValues>
 <상의>탑, 블라우스, 티셔츠, 니트웨어, 셔츠, 브라탑, 후드티</상의>
 <하의>: 청바지, 팬츠, 스커트, 레깅스, 조거팬츠</하의>
@@ -42,12 +59,7 @@ attributes:
 <신발>운동화, 구두, 샌들</신발>
 <악세서리>주얼리, 기타, 모자</악세서리>
 <가방>가방, 백팩, 힙색</가방>
-</parent category>
-
-rule:
-<clothes><order></order><info></info></clothes>
-The values wrapped in <order> are the order of the clothing photos.
-The information wrapped in <info> is an attribute that has already been identified for each garment. If the identified information is given, please refer to it.
+</possibleValues>
 
 Response Format:
 Please return it in JSON format as in the following example.
@@ -97,16 +109,18 @@ Please return it in JSON format as in the following example.
 
     def parse_response(self, response: dict) -> dict[int, Attributes]:
         transformed_data = {}
-        last_id=0
         for id in response.keys():
             value = response[id]
             transformed_item = {k: self.transform(k, v) for k, v in value.items()}
             transformed_item["categoryLowId"] = transformed_item["category"]
             del transformed_item["category"]
 
+            for k,v in transformed_item.items():
+                if v == "None":
+                    transformed_item[k]=None
+
             transformed_data[int(id)] = Attributes(**transformed_item)
-            last_id=int(id)
-        logging.info("id: "+str(last_id)+" attribute extracted -> "+str(transformed_data[last_id]))
+        logging.info("Attribute extracted -> "+str(transformed_data))
         return transformed_data
 
     def get_response(self) -> dict[Any, Any]:
@@ -161,7 +175,7 @@ class ExtractAttributesURL(ExtractAttribute):
                 }
             }, {
                 "type": "text",
-                "text": f"<clothes><order>{data.clothId}</order><info>{data.category + (',' + data.color if data.color else '')}</info></clothes>"
+                "text": f"<clothes><order>{data.clothId}</order><type>{data.category}</type>{('<color>' + data.color+'</color>') if data.color else ''}</clothes>"
             }])
         return content_list
 
@@ -171,10 +185,11 @@ class ExtractAttributesURL(ExtractAttribute):
 
 class ExtractAttributesImage(ExtractAttribute):
     image: UploadFile
+    type: str
 
-    def __init__(self, image: UploadFile):
+    def __init__(self, image: UploadFile, type:str=""):
         self.image = image
-
+        self.type=type
     def make_user_content(self) -> list:
         return [{
             "type": "image_url",
@@ -183,7 +198,7 @@ class ExtractAttributesImage(ExtractAttribute):
             }
         }, {
             "type": "text",
-            "text": f"<clothes><order>{0}</order><info></info></clothes>"
+            "text": f"<clothes><order>{0}</order><type>{type}</type></clothes>"
         }]
 
     def get_result(self):
