703fb91f91c6688e1ebb44faaea2c3e3a4fa542c
dana019 <dana019@naver.com>
2024-08-13 10:24:41 +0900

    fix: puppeteer browser 옵션 수정

--- frontend/ozz/src/app/apis/ably-login/route.ts
+++ frontend/ozz/src/app/apis/ably-login/route.ts
@@ -73,6 +73,13 @@ const fetchOrderDataUsingPuppeteer = async (
 ): Promise<OrderData[]> => {
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath: '/usr/bin/chromium-browser',
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+    ],
   })
   const page = await browser.newPage()
 
