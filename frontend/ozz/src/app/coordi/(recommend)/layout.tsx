feb219c2f6e40a35e6e0d59da7a4881863590c62
이보연 <btothey99@ewhain.net>
2024-08-13 16:39:51 +0900

    feat: 코디북 버튼 추천코디에 추가
    Jira: S11P12A804-386

--- frontend/ozz/src/app/coordi/(recommend)/layout.tsx
+++ frontend/ozz/src/app/coordi/(recommend)/layout.tsx
@@ -1,15 +1,19 @@
 import Header from '@/components/Header'
+import HeaderButton from '@/components/Button/HeaderButton'
 import { WeatherProvider } from '@/contexts/WeatherContext'
+import { FaBookmark } from 'react-icons/fa6'
 
 export default function CoordiRecommendLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const rightButton = <HeaderButton icon={<FaBookmark />} href="/coordi/book" />
+
   return (
     <WeatherProvider>
       <section>
-        <Header title="추천 코디" />
+        <Header title="추천 코디" rightButton={rightButton} />
         {children}
       </section>
     </WeatherProvider>
