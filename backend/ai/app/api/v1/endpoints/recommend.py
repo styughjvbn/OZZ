da3205039120d5726a77ec586eb8a5e75e65786d
SeoJongwon <3338761@naver.com>
2024-08-13 17:09:42 +0900

    feat: 단일 옷 속성 추출 추가

--- backend/ai/app/api/v1/endpoints/recommend.py
+++ backend/ai/app/api/v1/endpoints/recommend.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from fastapi import APIRouter, Header, Depends
 
 from app.schemas.recommend import Consider, RecommendationsResponse
@@ -6,6 +8,6 @@ from app.services.recommend import RecommendService
 router = APIRouter(prefix="/recommend", tags=["Recommendations"])
 
 
-@router.post("", response_model=list[RecommendationsResponse])
+@router.post("", response_model=Union[list[RecommendationsResponse]|None])
 async def extract_attributes(consider: Consider,x_user_id:str = Header(default=None),service:RecommendService=Depends(RecommendService)):
     return service.get_recommend_outfit(x_user_id,consider)
