59b3c9189638d84b120fdaf7dffdea7aa9113a6e
이보연 <btothey99@ewhain.net>
2024-08-14 17:43:06 +0900

    style: 타입과 경로명 수정
    맵 타입과 경로명 수정
    
    Jira: S11P12A804-385

--- frontend/ozz/src/types/clothing.ts
+++ frontend/ozz/src/types/clothing.ts
@@ -535,6 +535,40 @@ export const categoryLowIdToHighNameMap: { [key: number]: string } = {
   30: '가방',
 }
 
+// 상위 카테고리 ID에서 하위 카테고리 ID로의 맵핑
+export const categoryIdMap: { [key: number]: number } = {
+  1: 1,
+  2: 1,
+  3: 1,
+  4: 1,
+  5: 1,
+  6: 1,
+  7: 1,
+  8: 2,
+  9: 2,
+  10: 2,
+  11: 2,
+  12: 2,
+  13: 3,
+  14: 3,
+  15: 3,
+  16: 3,
+  17: 3,
+  18: 3,
+  19: 3,
+  20: 4,
+  21: 4,
+  22: 5,
+  23: 5,
+  24: 5,
+  25: 6,
+  26: 6,
+  27: 6,
+  28: 7,
+  29: 7,
+  30: 7,
+}
+
 export const colorMap: { [key: string]: string } = {
   WHITE: '흰색',
   BLACK: '검정',
