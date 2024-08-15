264b69249b853ef6131578c18c761b3d9d2bfe18
SeoJongwon <3338761@naver.com>
2024-08-15 15:31:27 +0900

    feat: 이미지 base64변경 및 url 이미지 다운로드 유틸 추가

--- backend/ai/app/utils/image_utils.py
+++ backend/ai/app/utils/image_utils.py
@@ -1,3 +1,4 @@
+import requests
 from fastapi import UploadFile
 from rembg import remove
 from io import BytesIO
@@ -25,8 +26,19 @@ def remove_background_and_encode(image_file: UploadFile) -> str:
     return base64_encoded_image
 
 
+def image_to_base64str(image: Image):
+    # 결과 이미지를 바이트 스트림으로 변환
+    output_stream = BytesIO()
+    image.save(output_stream, format="PNG")
+    output_stream.seek(0)
+
+    # 바이트 스트림을 base64로 인코딩
+    base64_encoded_image_str = base64.b64encode(output_stream.read()).decode('utf-8')
+    return base64_encoded_image_str
+
+
 def resize_and_convert_to_png(image: Image) -> Image:
-    max_size = 512
+    max_size = 300
     width, height = image.size
 
     # 긴 변을 기준으로 비율을 유지하면서 크기 조정
@@ -48,3 +60,9 @@ def resize_and_convert_to_png(image: Image) -> Image:
         image = image.convert("RGB")
 
     return image
+
+
+def download_img(image_url: str) -> Image:
+    # 이미지 다운로드
+    response = requests.get(image_url)
+    return Image.open(BytesIO(response.content))
