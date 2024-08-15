5baf9ae307566ca5405ce437e5f60e7066ea0653
이보연 <btothey99@ewhain.net>
2024-08-15 15:37:43 +0900

    feat: 가상피팅 코디 이미지 생성 수정
    Jira: S11P12A804-402

--- frontend/ozz/src/components/Button/ShareCommunityButton.tsx
+++ frontend/ozz/src/components/Button/ShareCommunityButton.tsx
@@ -1,9 +1,17 @@
 import { FaShareAlt } from 'react-icons/fa'
 import { Button } from '../ui/button'
 
-export default function ShareCommunityButton() {
+type ShareCommunityButtonProps = {
+  onClick: () => void
+  disabled: boolean
+}
+
+export default function ShareCommunityButton({
+  onClick,
+  disabled,
+}: ShareCommunityButtonProps) {
   return (
-    <Button variant="line" className="w-48 h-9 p-4">
+    <Button variant="line" className="w-48 h-9 p-4" disabled={disabled}>
       {' '}
       <FaShareAlt className="text-primary-400 mr-2" /> 커뮤니티에 공유하기{' '}
     </Button>
--- frontend/ozz/src/containers/fitting-page/index.tsx
+++ frontend/ozz/src/containers/fitting-page/index.tsx
@@ -5,6 +5,7 @@
 import { useState, useRef } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import html2canvas from 'html2canvas'
 
 import styles from '@/styles/FittingPage.module.css'
@@ -84,6 +85,7 @@ export default function FittingContainer() {
   const [styleList, setStyleList] = useState<Style[]>([]) // 스타일 태그
   const fittingContainerRef = useRef<HTMLDivElement | null>(null)
   const [previewUrl, setPreviewUrl] = useState<string | null>(null)
+  const router = useRouter()
 
   const handleAddItem = (category: string) => {
     // + 버튼을 눌렀을 때
@@ -212,18 +214,42 @@ export default function FittingContainer() {
     // 2. 이미지 생성 전에 배경색 적용
     fittingContainerRef.current!.style.backgroundColor = randomBackgroundColor
 
+    // 3. null 이미지를 가진 요소 숨기기
+    const hiddenElements: HTMLElement[] = []
+    fittingItems.forEach((item, index) => {
+      if (item.image === null) {
+        const element = fittingContainerRef.current!.querySelectorAll(
+          '.' + styles.clothingItem,
+        )[index] as HTMLElement
+
+        const imageElement = element.querySelector('img') as HTMLImageElement
+        if (imageElement) {
+          imageElement.style.opacity = '0' // 이미지가 투명해지도록 설정
+        }
+        hiddenElements.push(imageElement)
+      }
+    })
+
     try {
       const width = 900 // 9:16 비율의 가로 크기
       const height = 1600 // 9:16 비율의 세로 크기
 
-      // 3. HTML을 캔버스로 변환
+      // 4. HTML을 캔버스로 변환
       const canvas = await html2canvas(fittingContainerRef.current!, {
         width,
         height,
         ignoreElements: (element) => element.tagName === 'BUTTON', // BUTTON 태그 무시
       })
 
-      // 4. 캔버스를 Blob (이미지 파일)로 변환
+      // const dataUrl = canvas.toDataURL('image/png')
+      // setPreviewUrl(dataUrl)
+
+      // 5. 숨겨진 요소 다시 표시
+      hiddenElements.forEach((element) => {
+        element.style.opacity = '0.1'
+      })
+
+      // 6. 캔버스를 Blob (이미지 파일)로 변환
       canvas.toBlob(async (blob) => {
         fittingContainerRef.current!.style.backgroundColor =
           originalBackgroundColor
@@ -236,18 +262,16 @@ export default function FittingContainer() {
           type: 'image/png',
         })
 
-        // 5. 코디 정보를 생성
-        // 6. API 요청 데이터 준비
+        // 7. 코디 정보 생성 후 API 요청 데이터 준비
         const payload: CreateCoordinatePayload = {
           imageFile,
           request: coordinateRequest,
         }
 
-        console.log('payload ', payload)
+        // console.log('payload ', payload)
 
         try {
           const coordinateId = await createCoordinate(payload)
-          // 성공 처리 (예: 확인 모달 띄우기, 페이지 이동 등)
 
           // 코디북 조회
           const favoriteGroups = await getFavoritesGroupListOfUsers()
@@ -274,7 +298,7 @@ export default function FittingContainer() {
             targetFavoriteGroupId,
             coordinateId,
           )
-          console.log('response ', response)
+          // console.log('response ', response)
           setIsConfirmModalOpen(true)
         } catch (error) {
           console.error('코디 생성 실패:', error)
@@ -286,6 +310,11 @@ export default function FittingContainer() {
       alert('코디 저장 중 오류가 발생했습니다.')
       fittingContainerRef.current!.style.backgroundColor =
         originalBackgroundColor
+
+      // 5. 숨겨진 요소 다시 표l (오류 발생 시에도 복원)
+      hiddenElements.forEach((element) => {
+        element.style.opacity = '0.1'
+      })
     }
   }
 
@@ -308,12 +337,18 @@ export default function FittingContainer() {
   const handleConfirm = () => {
     // 코디북 페이지로 이동?
     console.log('저장 완료!')
+    closeModal()
+    router.push('/coordi/book')
   }
 
   return (
     <>
-      <div className="flex flex-col">
-        <div className="w-full p-4" ref={fittingContainerRef}>
+      <div className="flex flex-col items-center justify-center">
+        {/* <div className="w-full p-4" ref={fittingContainerRef}> */}
+        <div
+          className="relative w-full max-w-[360px] h-auto aspect-w-9 aspect-h-16"
+          ref={fittingContainerRef}
+        >
           <div className={styles.clothingGrid}>
             {fittingItems.map((item) => (
               <div
@@ -374,107 +409,109 @@ export default function FittingContainer() {
           </div>
         </div>
       </div>
-      <div className="w-full max-w-md mt-4">
-        <h2 className="text-xl font-semibold pl-10 m-2">선택한 옷</h2>
-        {selectedClothes.length > 0 ? (
-          <ul className="mt-2 px-6">
-            {selectedClothes.map((item) => (
-              <li key={item.clothesId} className="mb-2 p-3 border-b">
-                <div className="flex items-center cursor-pointer">
-                  <Link href={`/closet/modify/${item.clothesId}`} passHref>
-                    <div className="flex justify-center items-center w-16 h-16 bg-gray-light mr-4">
-                      <Image
-                        src={item.imageUrl}
-                        alt={item.name ?? 'No name'}
-                        width={75}
-                        height={75}
-                        style={{
-                          maxWidth: '100%',
-                          maxHeight: '100%',
-                          width: 'auto',
-                          height: 'auto',
-                          objectFit: 'contain',
+      <div className="flex justify-center">
+        <div className="w-full max-w-md mt-4">
+          <h2 className="text-xl font-semibold pl-10 m-2">선택한 옷</h2>
+          {selectedClothes.length > 0 ? (
+            <ul className="mt-2 px-6">
+              {selectedClothes.map((item) => (
+                <li key={item.clothesId} className="mb-2 p-3 border-b">
+                  <div className="flex items-center cursor-pointer">
+                    <Link href={`/closet/modify/${item.clothesId}`} passHref>
+                      <div className="flex justify-center items-center w-16 h-16 bg-gray-light mr-4">
+                        <Image
+                          src={item.imageUrl}
+                          alt={item.name ?? 'No name'}
+                          width={75}
+                          height={75}
+                          style={{
+                            maxWidth: '100%',
+                            maxHeight: '100%',
+                            width: 'auto',
+                            height: 'auto',
+                            objectFit: 'contain',
+                          }}
+                        />
+                      </div>
+                    </Link>
+                    <div className="flex flex-row w-full justify-between">
+                      <div className="flex flex-col">
+                        <p className="mb-2">{item.categoryLow?.name}</p>
+                        <p className="text-md font-semibold">{item.name}</p>
+                      </div>
+                      <button
+                        type="button"
+                        aria-label="제거"
+                        onClick={() => {
+                          const categoryLowId = item.categoryLow?.categoryLowId
+                          if (categoryLowId !== undefined) {
+                            const highCategoryName =
+                              categoryLowIdToHighNameMap[categoryLowId]
+                            handleRemoveItem(highCategoryName)
+                          } else {
+                            console.error('카테고리 ID가 유효하지 않습니다.')
+                          }
                         }}
-                      />
+                        className="flex items-center justify-center bg-secondary text-primary-400 rounded-full h-6 w-6"
+                      >
+                        <FaMinus />
+                      </button>
                     </div>
-                  </Link>
-                  <div className="flex flex-row w-full justify-between">
-                    <div className="flex flex-col">
-                      <p className="mb-2">{item.categoryLow?.name}</p>
-                      <p className="text-md font-semibold">{item.name}</p>
-                    </div>
-                    <button
-                      type="button"
-                      aria-label="제거"
-                      onClick={() => {
-                        const categoryLowId = item.categoryLow?.categoryLowId
-                        if (categoryLowId !== undefined) {
-                          const highCategoryName =
-                            categoryLowIdToHighNameMap[categoryLowId]
-                          handleRemoveItem(highCategoryName)
-                        } else {
-                          console.error('카테고리 ID가 유효하지 않습니다.')
-                        }
-                      }}
-                      className="flex items-center justify-center bg-secondary text-primary-400 rounded-full h-6 w-6"
-                    >
-                      <FaMinus />
-                    </button>
                   </div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p className="text-gray-dark text-center mb-10">
-            아직 추가된 옷이 없어요
-          </p>
-        )}
-        <div className="mt-4 flex justify-around">
-          <SaveCoordiButton
-            onClick={handleSaveCoordi}
-            disabled={selectedClothes.length === 0}
-          />
-          <ShareCommunityButton />
-        </div>
-        {/* 미리보기 영역 */}
-        {previewUrl && (
-          <div className="mt-4">
-            <h2 className="text-xl font-semibold">코디 미리보기</h2>
-            <Image
-              src={previewUrl}
-              alt="코디 미리보기"
-              width={300}
-              height={300}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-gray-dark text-center mb-10">
+              아직 추가된 옷이 없어요
+            </p>
+          )}
+          <div className="mt-4 flex justify-around">
+            <SaveCoordiButton
+              onClick={handleSaveCoordi}
+              disabled={selectedClothes.length === 0}
             />
+            <ShareCommunityButton onClick={closeModal} disabled={true} />
           </div>
-        )}
-        {isCoordiModalOpen && (
-          <Modal onClose={() => setIsCoordiModalOpen(false)}>
-            <CoordiNameModal
-              setValue={handleCoordiNameSubmit}
-              onClose={closeModal}
-            />
-          </Modal>
-        )}
-        {isStyleModalOpen && (
-          <Modal onClose={() => setIsStyleModalOpen(false)}>
-            <CoordiStyleModal
-              setValue={handleStyleSubmit}
-              onPrev={handlePrevFromStyle}
-              onClose={closeModal}
-            />
-          </Modal>
-        )}
-        {isConfirmModalOpen && (
-          <Modal onClose={() => setIsConfirmModalOpen(false)}>
-            <ConfirmModal
-              onClose={() => setIsConfirmModalOpen(false)}
-              message="코디북에 저장되었습니다!"
-              onConfirm={closeModal}
-            />
-          </Modal>
-        )}
+          {/* 미리보기 영역 */}
+          {previewUrl && (
+            <div className="mt-4">
+              <h2 className="text-xl font-semibold">코디 미리보기</h2>
+              <Image
+                src={previewUrl}
+                alt="코디 미리보기"
+                width={300}
+                height={300}
+              />
+            </div>
+          )}
+          {isCoordiModalOpen && (
+            <Modal onClose={() => setIsCoordiModalOpen(false)}>
+              <CoordiNameModal
+                setValue={handleCoordiNameSubmit}
+                onClose={closeModal}
+              />
+            </Modal>
+          )}
+          {isStyleModalOpen && (
+            <Modal onClose={() => setIsStyleModalOpen(false)}>
+              <CoordiStyleModal
+                setValue={handleStyleSubmit}
+                onPrev={handlePrevFromStyle}
+                onClose={closeModal}
+              />
+            </Modal>
+          )}
+          {isConfirmModalOpen && (
+            <Modal onClose={() => setIsConfirmModalOpen(false)}>
+              <ConfirmModal
+                onClose={() => setIsConfirmModalOpen(false)}
+                message="코디북에 저장되었습니다!"
+                onConfirm={handleConfirm}
+              />
+            </Modal>
+          )}
+        </div>
       </div>
 
       <ClosetSidebar
